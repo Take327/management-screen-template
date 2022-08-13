09ce60c899c9aed1e6fa6af5a073451f01da965b
Take327 <gatewayseed2@gmail.com>
2022-08-13 12:53:10 +0900

    Redux 実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header';
 import './App.css';
+import { useAppSelector } from './redux/hooks';
 
 function App() {
+    const state = useAppSelector((state) => state.commonState.sideNaviOpen);
+
     return (
         <div className="App">
             <Header />
             <div>
-                <div></div>
+                <div>{state ? 'true' : 'false'}</div>
             </div>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const rootElement = document.getElementById('root');
+// https://blog.logrocket.com/how-to-use-typescript-with-react-18-alpha/
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
 root.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
